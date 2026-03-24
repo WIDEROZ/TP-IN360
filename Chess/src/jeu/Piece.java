e311512605c11b52a43b737a89b0982921d03fef
H-Boit <boitet_hugo@hotmail.com>
2026-03-24 10:00:28 +0100

    Update Piece.java

--- Chess/src/jeu/Piece.java
+++ Chess/src/jeu/Piece.java
@@ -2,8 +2,15 @@ package jeu;
 
 public abstract class Piece {
 
-	private boolean couleur;
-	private boolean dejaJoue;
+	protected boolean couleur;
+	protected boolean dejaJoue;
+	
+	/**
+	 * Couleur : 0 pour les blancs, 1 pour les noirs
+	 */
+	public Piece(boolean couleur) {
+		this.couleur = couleur;
+	}
 	
 	public boolean getCouleur() {
 		return couleur;
@@ -21,11 +28,22 @@ public abstract class Piece {
 		this.dejaJoue = dejaJoue;
 	}
 	
-	public abstract void deplacement();
-	public abstract void prendre();
+	/**
+	 * Retourne un boolean : true si le deplacement est valide.
+	 * Paramètres :
+	 * indexDepart (entier representant la position de depart du pion); 
+	 * indexArrive (entier representant la position d'arrive du pion). 
+	 * NB : Un index de position est de la forme intColonneintLigne. ex : a1 -> 11, b1 -> 21
+	 * 
+	 */
+	public abstract boolean deplacementEstOk(int indexDepart, int indexArrive);
+	
+	/**
+	 * Affiche la piece
+	 */
+	public abstract void afficherPiece();
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
 	}
 
