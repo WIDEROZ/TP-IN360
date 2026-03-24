1bbd438c3add528c701b0f12c0af51edfef83306
H-Boit <boitet_hugo@hotmail.com>
2026-03-24 09:01:08 +0100

    Add files via upload
    Idée de départ

---
+++ Chess/src/jeu/Piece.java
@@ -0,0 +1,32 @@
+package jeu;
+
+public abstract class Piece {
+
+	private boolean couleur;
+	private boolean dejaJoue;
+	
+	public boolean getCouleur() {
+		return couleur;
+	}
+
+	public void setCouleur(boolean couleur) {
+		this.couleur = couleur;
+	}
+
+	public boolean getDejaJoue() {
+		return dejaJoue;
+	}
+
+	public void setDejaJouer(boolean dejaJoue) {
+		this.dejaJoue = dejaJoue;
+	}
+	
+	public abstract void deplacement();
+	public abstract void prendre();
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ Chess/src/jeu/Pion.java
@@ -0,0 +1,12 @@
+package jeu;
+
+public class Pion extends Piece{
+	
+	public void deplacement() {
+		
+	}
+	
+	public void prendre() {
+		
+	}
+}
