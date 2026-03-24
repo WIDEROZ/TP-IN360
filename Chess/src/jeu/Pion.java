aa34e4641717602a5a6ca4be6b2a09bde1bb8a59
H-Boit <boitet_hugo@hotmail.com>
2026-03-24 09:59:52 +0100

    Update Pion.java

--- Chess/src/jeu/Pion.java
+++ Chess/src/jeu/Pion.java
@@ -2,11 +2,27 @@ package jeu;
 
 public class Pion extends Piece{
 	
-	public void deplacement() {
-		
+	/**
+	 * Couleur : 0 pour les blancs, 1 pour les noirs
+	 */
+	public Pion(boolean couleur) {
+		super(couleur);
+		dejaJoue = false;
 	}
 	
-	public void prendre() {
-		
+	public boolean deplacementEstOk(int indexDepart, int indexArrive) {
+		boolean deplacementOk = false;
+		if (indexDepart != indexArrive && dejaJoue == false && (indexArrive == indexDepart + 2)) {
+			dejaJoue = true;
+			deplacementOk = true;
+		}
+		else if(indexDepart != indexArrive && dejaJoue == true && (indexArrive == indexDepart + 2)) {
+			deplacementOk = true;
+		}
+		return deplacementOk;
+	}
+	
+	public void afficherPiece() {
+		System.out.print("p" + couleur);
 	}
 }
